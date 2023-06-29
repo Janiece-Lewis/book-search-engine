0d96e3a2d0d6aa8a4d44054b7d65f80a1bdfefd7
Janiece-Lewis <jlewilewi@gmail.com>
2023-06-29 17:49:07 -0400

    updated typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,12 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
+const { User} = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('books');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
+        return User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -48,29 +35,14 @@ const resolvers = {
 
       return { token, user };
     },
-    addThought: async (parent, { thoughtText }, context) => {
+    
+    saveBook: async (parent, { bookInfo }, context) => {
       if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
+        return User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
           {
             $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
+                savedBooks: bookInfo 
             },
           },
           {
@@ -81,31 +53,15 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    removeThought: async (parent, { thoughtId }, context) => {
+    
+    removeBook: async (parent, {bookId}, context) => {
       if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
+        return User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
           {
             $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
+              savedBooks: {
+                bookId
               },
             },
           },
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,32 +6,33 @@ type Query {
 type Mutation {
     login (email: String!, password: String!): Auth
     addUser (username: String!, email:String!,password: String!): Auth
-	// saveBook: [Authors](
-	// input BookInput 
-	// 	authors: [String!]!,
-	// 	description: String!,
-	// 	title: String!,
-	// 	bookId: ID!,
-	// 	image: String!,
-	// 	link: String!)
-	
+	saveBook (bookInfo:BookInput):User	
 removeBook(bookId: ID!): User
+}
 
 type User {
     _id: ID
     username: String
     email: String
-    // bookCount: Number
-    savedBooks: [Book]!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
 type Book {
-	authors: [String!]!
-	description: String!
+	authors: [String]
+	description: String
 	title: String!
 	bookId: ID!
-	image: String!
-	link: String!)
+	image: String
+	link: String
+  }
+  input BookInput {
+	authors: [String]
+	description: String
+	title: String!
+	bookId: ID!
+	image: String
+	link: String
   }
 
 type Auth {
@@ -39,51 +40,5 @@ type Auth {
     user: User
   }
 `;
-
-// * `typeDefs.js`: Define the necessary `Query` and `Mutation` types:
-
-
-
-// 		* `Mutation` type:
-
-// 			* `login`: Accepts an email and password as parameters; returns an `Auth` type.
-
-// 			* `addUser`: Accepts a username, email, and password as parameters; returns an `Auth` type.
-
-// 			* `saveBook`: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
-
-// 			* `removeBook`: Accepts a book's `bookId` as a parameter; returns a `User` type.
-
-// * `User` type:
-
-// 	* `_id`
-
-// 	* `username`
-
-// 	* `email`
-
-// 	* `bookCount`
-
-// 	* `savedBooks` (This will be an array of the `Book` type.)
-
-// * `Book` type:
-
-// 	* `bookId` (Not the `_id`, but the book's `id` value returned from Google's Book API.)
-
-// 	* `authors` (An array of strings, as there may be more than one author.)
-
-// 	* `description`
-
-// 	* `title`
-
-// 	* `image`
-
-// 	* `link`
-
-// * `Auth` type:
-
-// 	* `token`
-
-// 	* `user` (References the `User` type.)
 
 module.exports = typeDefs;
